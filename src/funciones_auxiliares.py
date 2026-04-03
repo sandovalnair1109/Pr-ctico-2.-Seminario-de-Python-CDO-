a11e0bc9076e9158f60e8eaff6a8c45f4640826f
Nair Sandoval <sandovalnair683@gmail.com>
2026-04-03 11:39:18 -0300

    Quinto ejercicio: calculadora de costo de envío

--- src/funciones_auxiliares.py
+++ src/funciones_auxiliares.py
@@ -73,3 +73,36 @@ def validar_email (email):
     
     #Si pasó todos los criterios
     return False
+
+def calcular_costo_envio(peso, zona):
+    """
+    Calcula el costo de envío según peso y zona.
+    Parámetros:
+        peso:número en kg
+        zona:string 'local', 'regional' o 'nacional'
+    Retorna:
+        costo (int) si la zona es válida
+        None si la zona no lo es
+    """
+    #verificamos si la zona es válida
+    zonas_validas = ['local', 'regional','nacional']
+    if zona.lower() not in zonas_validas:
+        return None
+    
+    #determinar rango de peso
+    if peso <= 1:
+        rango= 'hasta_1kg'
+    elif peso <= 5:
+        rango= '1_a_5kg'
+    else:
+        rango= 'mas_de_5kg'
+    
+    #tabla de precios
+    precios= {
+        'local':{'hasta_1kg':500 , '1_a_5kg':1000, 'mas_de_5kg':2000},
+        'regional':{'hasta_1kg':1000 , '1_a_5kg':2500, 'mas_de_5kg':5000},
+        'nacional':{'hasta_1kg':2000 , '1_a_5kg':4500, 'mas_de_5kg':8000}
+    }
+    #accedo primero a la zona si correcta, y luego al rango (diccionario dentro de diccionario)
+    return precios[zona.lower()][rango]
+
